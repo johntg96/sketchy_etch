3bb91e57b67a84dfc01f6ae3013c72a67856793c
johntg96 <johntg@protonmail.com>
2019-10-20 20:08:07 -0500

    div generation created at primitive level. Mild css styling applied to generated divs.

--- sketch.js
+++ sketch.js
@@ -1,1 +1,10 @@
 // sketch.js
+const grid = document.querySelector('#gridContainer');
+
+for (i = 0; i < 16; i++) {
+	var div = document.createElement('div');
+	div.classList.add('gridSquare');
+	console.log(i);
+	grid.appendChild(div);
+}
+
