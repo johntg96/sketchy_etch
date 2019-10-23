d597dbaa460be53e6a16bbd1e30cb09d8b3cd92f
johntg96 <johntg@protonmail.com>
2019-10-22 19:43:50 -0500

    major restyling update. CSS animations added. May need to refactor code to boost performance when dealing with larger grids especially!

--- sketch.js
+++ sketch.js
@@ -2,28 +2,26 @@
 const container = document.querySelector('#gridContainer');
 
 function createGrid(gridSize) {
-	const dynamicW = 50 / gridSize + "em";
-	const dynamicH = 50 / gridSize + "em";
+	const dynamicSize = 50 / gridSize + "em";
 
 	for (boxNum = 0; boxNum < gridSize * gridSize; boxNum++) {
 		var square = document.createElement('div');
-		square.style.cssText = 'height: ' + dynamicH + '; width: ' + dynamicW + ';';
+		square.style.cssText = 'height: ' + dynamicSize + '; width: ' + dynamicSize + ';';
 		square.classList.add('gridSquare');
 		square.id = "square" + boxNum;
-		square.setAttribute('onmouseover', 'changeColor(this.id)');
+		square.setAttribute('onmouseover', 'draw(this.id)');
 		container.appendChild(square);
 	}	
 }
 
-createGrid(60);
+createGrid(50);
 
-function changeColor(id) {
-	document.querySelector('#' + String(id)).style.backgroundColor = "green";
+function draw(id) {
+	document.querySelector('#' + String(id)).classList.add('drawn');
 }
 
 // TO DO:
-// figure out how to dynamically size squares in grid..
-// The *container* grid should be staying the same size, while the squares inside change width + height based on how many squares are being created *x*
-// currently the squares ignore the container and will outgrow their container
-// the squares are being given a calculated height and width based on the amount of squares being created, but not in relation to how big the -
-// grid containers dimensions are
+// add grid size creation button
+// add ability for user to pick a drawing color
+// style page better
+// make page dynamic and mobile friendly
