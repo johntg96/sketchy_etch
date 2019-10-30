0498b1a78bd9b99a59794674acb42c61628118b2
johntg96 <johntg@protonmail.com>
2019-10-29 20:24:54 -0500

    reset button now only removes 'drawn' class from squares instead of the inefficient delete and re-create approach.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 // sketch.js
 var container = document.querySelector('#gridContainer');
-var gridSize = 8;
+var gridSize = 16;
 
 function createGrid(gridSize) {
 	const dynamicSize = 45 / gridSize + "em";
@@ -12,7 +12,7 @@ function createGrid(gridSize) {
 		square.id = "square" + boxNum;
 		square.setAttribute('onmouseover', 'draw(this.id)');
 		container.appendChild(square);
-	}	
+	}
 }
 
 createGrid(gridSize);
@@ -21,23 +21,20 @@ function draw(id) {
 	document.querySelector('#' + String(id)).classList.add('drawn');
 }
 
-function reset(sizeOfGrid) {
-	// reset grid using this function..
-	// delete all squares and re-run 'createGrid' function
+function newGrid(sizeOfGrid) {
 	document.querySelectorAll('.gridSquare').forEach(e => e.remove());
 	createGrid(gridSize);
 }
 
 document.querySelector('#reset').addEventListener('click', function(){
-	reset(gridSize);
+	document.querySelectorAll('.gridSquare').forEach(e => e.classList.remove('drawn'));
 });
 
 document.querySelector('#new').addEventListener('click', function(){
 	var newGridSize = prompt('Size of new grid will be "x * x"\n\nEnter size of new grid (x): ');
 	gridSize = newGridSize;
-	reset(gridSize);
+	newGrid(gridSize);
 });
-
 
 // TO DO
 // Optimize! The goal of this project has been acheived..
